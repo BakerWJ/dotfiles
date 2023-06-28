491a3c4149fdcc79413fde8170763dc696f9b0db
Baker Jackson <bakerwjackson@gmail.com>
2023-06-27 17:00:03 -0700

    add neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,283 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+
+require("lazy").setup({
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
+	{
+		"numToStr/Comment.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPost", "BufNewFile" },
+		build = ":TSUpdate",
+	},
+	{
+		"ggandor/leap.nvim",
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+			{ "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+		},
+		config = function()
+			require("leap").add_default_mappings()
+		end,
+	},
+	"tpope/vim-sleuth",
+	"nathom/filetype.nvim",
+	{
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons" },
+		opts = {},
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		opts = {},
+	},
+	{
+		"ibhagwan/fzf-lua",
+		-- optional for icon support
+		requires = { "nvim-tree/nvim-web-devicons" },
+		keys = {
+			{ "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>" },
+			{ "<leader>fg", "<cmd>lua require('fzf-lua').live_grep_native()<CR>" },
+			{ "<leader>fb", "<cmd>lua require('fzf-lua').buffers()<CR>" },
+		},
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		event = "VeryLazy",
+		keys = {
+			{ "<C-n>", "<cmd>NvimTreeToggle<CR>", mode = { "n" } },
+		},
+		opts = {
+			hijack_cursor = true,
+			git = { enable = false, ignore = true },
+			filters = { custom = { ".git", ".idea", ".cache", ".DS_Store", "__pycache__" } },
+			actions = {
+				open_file = {
+					quit_on_open = true,
+				},
+			},
+		},
+	},
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ -- Optional
+				"williamboman/mason.nvim",
+				build = ":MasonUpdate",
+			},
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+			-- extra nvim-cmp sources
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+		config = function()
+			-- null_ls setup
+			local lsp_formatting = function(bufnr)
+				vim.lsp.buf.format({
+					filter = function(client)
+						-- apply whatever logic you want (in this example, we'll only use null-ls)
+						return client.name == "null-ls"
+					end,
+					bufnr = bufnr,
+				})
+			end
+
+			vim.keymap.set("n", "ff", lsp_formatting, { noremap = true })
+
+			-- if you want to set up formatting on save, you can use this as a callback
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+			-- add to your shared on_attach callback
+			local null_ls_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							lsp_formatting(bufnr)
+						end,
+					})
+				end
+			end
+
+			local lsp = require("lsp-zero").preset({})
+
+			lsp.on_attach(function(client, bufnr)
+				lsp.default_keymaps({ buffer = bufnr })
+				null_ls_attach(client, bufnr)
+			end)
+
+			lsp.skip_server_setup({ "jdtls" })
+
+			lsp.setup()
+
+			local cmp = require("cmp")
+
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+
+			local luasnip = require("luasnip")
+
+			cmp.setup({
+				sources = {
+					{ name = "path" },
+					{ name = "nvim_lsp" },
+					{ name = "buffer", keyword_length = 3 },
+					{ name = "luasnip", keyword_length = 2 },
+				},
+				mapping = {
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+						-- they way you will only jump inside the snippet region
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				},
+			})
+		end,
+	},
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		opts = function()
+			local null_ls = require("null-ls")
+
+			return {
+				sources = {
+					null_ls.builtins.diagnostics.eslint,
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.clang_format,
+				},
+			}
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		opts = {},
+	},
+	{
+		"stevearc/dressing.nvim",
+		opts = {},
+	},
+	"mfussenegger/nvim-jdtls",
+})
+
+vim.opt.encoding = "utf-8"
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.swapfile = false
+vim.opt.hlsearch = false
+vim.opt.autoread = true
+vim.opt.mouse = "a"
+vim.opt.autoindent = true
+vim.opt.showmode = false
+vim.opt.showtabline = 2
+vim.opt.hidden = true
+-- tabs
+vim.opt.tabstop = 4
+-- set leader
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+-- use system clipboard
+vim.opt.clipboard = "unnamedplus"
+-- lines visible while scrolling
+vim.opt.scrolloff = 7
+-- filetype checking
+vim.cmd("filetype plugin indent on")
+-- remove i from autocomplete
+vim.cmd("set complete-=i")
+vim.opt.incsearch = true
+vim.opt.wildmenu = true
+vim.opt.autoread = true
+vim.opt.history = 1000
+vim.cmd("set completeopt=menu,menuone,noselect")
+-- theme
+vim.opt.termguicolors = true
+vim.opt.syntax = "enable"
